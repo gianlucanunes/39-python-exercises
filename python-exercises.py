7868c61ed12b840a8657309f6cf22bfb5111caea
gianlucanunes <gianluca.nulima@gmail.com>
2025-02-04 11:01:35 -0300

    first-commit

---
+++ python-exercises.py
@@ -0,0 +1,70 @@
+## algorithm 1
+#num1 = float(input("Enter number 1: "))
+#num2 = float(input("Enter number 2: "))
+#sum = num1 + num2
+#print(f"{num1} + {num2} = {sum}")
+
+## algorithm 2
+#num1 = float(input("Enter number 1: "))
+#num2 = float(input("Enter number 2: "))
+#sum = num1 + num2
+#diff = num1 - num2
+#mult = num1 * num2
+#div = num1 / num2
+#print(f"{num1} + {num2} = {sum}\n{num1} - {num2} = {diff}\n{num1} * {num2} = {mult}\n{num1} / {num2} = {div}")
+
+## algorithm 3
+#distance = float(input("How long did you get with your car? (KM): "))
+#fuel = float(input("How much juice did you have before starting the journey? "))
+#averageConsumption = distance / fuel
+#print(f"You had to run a distance of {distance}km and had {fuel}L of juice begore going. You average consumption is {averageConsumption}L/KM.")
+
+## algorithm 4
+#name = input("Enter the salesman name: ")
+#fixedWage = float(input("Enter the salesman's wage: US$"))
+#sales = int(input("Enter the salesman's sales for this month: "))
+#salesValue = 1000
+#commision = 15 / 100
+#finalWage = fixedWage + ((salesValue * commision) * sales )
+#print(f"Name: {name}\nFixed wage: US${fixedWage}\nTotal of sales: {sales}\nSales value (each): US${salesValue}\nFinal wage: US${finalWage}")
+
+## algorithm 5 
+#name = input("Enter the student's name: ")
+#grade1 = float(input("Enter grade 1: "))
+#grade2 = float(input("Enter grade 2: "))
+#grade3 = float(input("Enter grade 3: "))
+#averageGrade = (grade1 + grade2 + grade3) / 3
+#print(f"Student name: {name}\nAverage grade: {averageGrade}")
+
+## algorithm 6
+#A = input("Enter the A value: ")
+#B = input("Enter the B value: ")
+#C = A
+#A = B
+#B = C
+#print(f"A value before: {B}\nA value now: {A}\nB value before: {A}\nB value now: {B}")
+
+## algorithm 7
+#celsiusTemp = float(input("Enter the temperature (in Celsius): "))
+#fahrenheitTemp = (9*celsiusTemp+160) / 5
+#print(f"Temperature (Celsius): {celsiusTemp}°c\nTemperature (Fahrenheit): {fahrenheitTemp}°f")
+
+## algorithm 8
+#dollar = float(input("Enter the amount of dollars: US$"))
+#dollarExchangeRate = float(input("Enter the exchange rate: "))
+#real = dollar * dollarExchangeRate
+#print(f"You have: US${dollar}\nThe exhange rate is: {dollarExchangeRate}\nYou have: R${real}")
+
+## algorithm 9
+#deposit = float(input("Enter the deposit value: US$"))
+#fee = 0.70 / 100
+#income = deposit + deposit * fee
+#print(f"The income is: US${income} (only 1 month)")
+
+## algorithm 10
+#sale = float(input("Enter the sale value: US$"))
+#invoice = sale / 5
+#invoiceIndex = 1
+#while invoiceIndex <= 5:
+#    print(f"Invoice {invoiceIndex}: US${invoice}")
+#    invoiceIndex+=1
